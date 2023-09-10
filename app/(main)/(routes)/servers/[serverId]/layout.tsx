47901208b61d2022153f8fd472fcb38c5967ea93
subhammishr <msubham193@gmail.com>
2023-09-10 20:58:55 +0530

    side bar created

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ServerIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+  
+};
+
+export default ServerIdLayout;
---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+import { Channel, ChannelType, Server } from "@prisma/client";
+
+export type ModalType =
+  | "createServer"
+  | "invite"
+  | "editServer"
+  | "members"
+  | "createChannel"
+  | "leaveServer"
+  | "deleteServer"
+  | "deleteChannel"
+  | "editChannel"
+  | "messageFile"
+  | "deleteMessage";
+
+interface ModalStore {
+  type: ModalType | null;
+  //   data: ModalData;
+  isOpen: boolean;
+  onOpen: (type: ModalType) => void;
+  onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+  type: null,
+  data: {},
+  isOpen: false,
+  onOpen: (type) => set({ isOpen: true, type }),
+
+  onClose: () => set({ isOpen: false, type: null }),
+}));
